342b085729ca21dbc67a726641389ba04bc877e7
maxwelli <117984247+maxwelldi@users.noreply.github.com>
2023-12-04 11:45:24 +0100

    add membership field to Customer model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+MEMBERSHIP_BRONZE = 'B'
+MEMBERSHIP_SILVER = 'S'
+MEMBERSHIP_GOLD = 'G'
+
+MEMBERSHIP_CHOICES = [
+  (MEMBERSHIP_BRONZE, 'Bronze'),
+  (MEMBERSHIP_SILVER, 'Silver'),
+  (MEMBERSHIP_GOLD, 'Gold'),
+]
+
 # Create your models here.
 class Product(models.Model):
   title = models.CharField(max_length=255)
@@ -16,3 +26,4 @@ class Customer(models.Model):
   email = models.EmailField(unique=True)
   phone = models.CharField(max_length=255)
   birth_date = models.DateField(null=True)
+  membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
